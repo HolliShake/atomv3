57711a98e49017f073a41169ab54bc84e52defa6
HolliShake <redondophilippandrewroa.dev@gmail.com>
2025-09-21 18:19:13 +0800

    Add object methods for key and value retrieval
    - Introduced new native functions `keys` and `values` to retrieve keys and values from objects, enhancing object manipulation capabilities.
    - Updated the `EXPORT_OBJECT` map to include the new methods.
    - Modified the test class to demonstrate usage of the new object methods, ensuring correct functionality.

--- runtime/builtin_object.go
+++ runtime/builtin_object.go
@@ -15,6 +15,34 @@ var obj_freeze = NewNativeFunc("freeze", 1, func(interpreter *AtomInterpreter, f
 	frame.Stack.Push(obj)
 })
 
+var obj_keys = NewNativeFunc("keys", 1, func(interpreter *AtomInterpreter, frame *AtomCallFrame, argc int) {
+	obj := frame.Stack.Pop()
+	if CheckType(obj, AtomTypeObj) {
+		keys := []*AtomValue{}
+		for key := range obj.Value.(*AtomObject).Elements {
+			keys = append(keys, NewAtomValueStr(key))
+		}
+		frame.Stack.Push(NewAtomValueArray(keys))
+	} else {
+		frame.Stack.Push(NewAtomValueError(FormatError(frame, "cannot keys non-object")))
+	}
+})
+
+var obj_values = NewNativeFunc("values", 1, func(interpreter *AtomInterpreter, frame *AtomCallFrame, argc int) {
+	obj := frame.Stack.Pop()
+	if CheckType(obj, AtomTypeObj) {
+		values := []*AtomValue{}
+		for _, value := range obj.Value.(*AtomObject).Elements {
+			values = append(values, value)
+		}
+		frame.Stack.Push(NewAtomValueArray(values))
+	} else {
+		frame.Stack.Push(NewAtomValueError(FormatError(frame, "cannot values non-object")))
+	}
+})
+
 var EXPORT_OBJECT = map[string]*AtomValue{
 	"freeze": NewAtomValueNativeFunc(obj_freeze),
+	"keys":   NewAtomValueNativeFunc(obj_keys),
+	"values": NewAtomValueNativeFunc(obj_values),
 }
