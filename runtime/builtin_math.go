75037bd03d5f0e6ec87059a9d0debd2ab3c86c93
HolliShake <redondophilippandrewroa.dev@gmail.com>
2025-09-21 18:36:57 +0800

    Add mathematical functions to the runtime math module
    - Introduced new native functions: `abs`, `floor`, `ceil`, `round`, `pow`, `sqrt`, and `log` to enhance the math module's capabilities.
    - Updated the `EXPORT_MATH` map to include the new functions for accessibility.
    - Modified the test class to demonstrate usage of the new mathematical functions, ensuring comprehensive testing of their functionality.

--- runtime/builtin_math.go
+++ runtime/builtin_math.go
@@ -1,6 +1,9 @@
 package runtime
 
-import "math/rand"
+import (
+	"math"
+	"math/rand"
+)
 
 var math_rand = NewNativeFunc("rand", 1, func(interpreter *AtomInterpreter, frame *AtomCallFrame, argc int) {
 	if argc != 1 {
@@ -12,7 +15,7 @@ var math_rand = NewNativeFunc("rand", 1, func(interpreter *AtomInterpreter, fram
 
 	arg := frame.Stack.Pop()
 
-	if !CheckType(arg, AtomTypeInt) {
+	if !IsNumberType(arg) {
 		frame.Stack.Push(NewAtomValueError(
 			FormatError(frame, "rand expects integer"),
 		))
@@ -23,6 +26,162 @@ var math_rand = NewNativeFunc("rand", 1, func(interpreter *AtomInterpreter, fram
 	frame.Stack.Push(NewAtomValueInt(rand.Intn(int(val))))
 })
 
+var math_abs = NewNativeFunc("abs", 1, func(interpreter *AtomInterpreter, frame *AtomCallFrame, argc int) {
+	if argc != 1 {
+		frame.Stack.Push(NewAtomValueError(
+			FormatError(frame, "abs expects 1 argument"),
+		))
+		return
+	}
+
+	arg := frame.Stack.Pop()
+
+	if !IsNumberType(arg) {
+		frame.Stack.Push(NewAtomValueError(
+			FormatError(frame, "abs expects number"),
+		))
+		return
+	}
+
+	val := CoerceToNum(arg)
+	frame.Stack.Push(NewAtomValueNum(math.Abs(val)))
+})
+
+var math_floor = NewNativeFunc("floor", 1, func(interpreter *AtomInterpreter, frame *AtomCallFrame, argc int) {
+	if argc != 1 {
+		frame.Stack.Push(NewAtomValueError(
+			FormatError(frame, "floor expects 1 argument"),
+		))
+		return
+	}
+
+	arg := frame.Stack.Pop()
+
+	if !IsNumberType(arg) {
+		frame.Stack.Push(NewAtomValueError(
+			FormatError(frame, "floor expects number"),
+		))
+		return
+	}
+
+	val := CoerceToNum(arg)
+	frame.Stack.Push(NewAtomValueNum(math.Floor(val)))
+})
+
+var math_ceil = NewNativeFunc("ceil", 1, func(interpreter *AtomInterpreter, frame *AtomCallFrame, argc int) {
+	if argc != 1 {
+		frame.Stack.Push(NewAtomValueError(
+			FormatError(frame, "ceil expects 1 argument"),
+		))
+		return
+	}
+
+	arg := frame.Stack.Pop()
+
+	if !IsNumberType(arg) {
+		frame.Stack.Push(NewAtomValueError(
+			FormatError(frame, "ceil expects number"),
+		))
+		return
+	}
+
+	val := CoerceToNum(arg)
+	frame.Stack.Push(NewAtomValueNum(math.Ceil(val)))
+})
+
+var math_round = NewNativeFunc("round", 1, func(interpreter *AtomInterpreter, frame *AtomCallFrame, argc int) {
+	if argc != 1 {
+		frame.Stack.Push(NewAtomValueError(
+			FormatError(frame, "round expects 1 argument"),
+		))
+		return
+	}
+
+	arg := frame.Stack.Pop()
+
+	if !IsNumberType(arg) {
+		frame.Stack.Push(NewAtomValueError(
+			FormatError(frame, "round expects number"),
+		))
+		return
+	}
+
+	val := CoerceToNum(arg)
+	frame.Stack.Push(NewAtomValueNum(math.Round(val)))
+})
+
+var math_pow = NewNativeFunc("pow", 2, func(interpreter *AtomInterpreter, frame *AtomCallFrame, argc int) {
+	if argc != 2 {
+		frame.Stack.Push(NewAtomValueError(
+			FormatError(frame, "pow expects 2 arguments"),
+		))
+		return
+	}
+
+	arg := frame.Stack.Pop()
+	arg2 := frame.Stack.Pop()
+
+	if !IsNumberType(arg) || !IsNumberType(arg2) {
+		frame.Stack.Push(NewAtomValueError(
+			FormatError(frame, "pow expects number"),
+		))
+		return
+	}
+
+	val := CoerceToNum(arg)
+	val2 := CoerceToNum(arg2)
+	frame.Stack.Push(NewAtomValueNum(math.Pow(val, val2)))
+})
+
+var math_sqrt = NewNativeFunc("sqrt", 1, func(interpreter *AtomInterpreter, frame *AtomCallFrame, argc int) {
+	if argc != 1 {
+		frame.Stack.Push(NewAtomValueError(
+			FormatError(frame, "sqrt expects 1 argument"),
+		))
+		return
+	}
+
+	arg := frame.Stack.Pop()
+
+	if !IsNumberType(arg) {
+		frame.Stack.Push(NewAtomValueError(
+			FormatError(frame, "sqrt expects number"),
+		))
+		return
+	}
+
+	val := CoerceToNum(arg)
+	frame.Stack.Push(NewAtomValueNum(math.Sqrt(val)))
+})
+
+var math_log = NewNativeFunc("log", 1, func(interpreter *AtomInterpreter, frame *AtomCallFrame, argc int) {
+	if argc != 1 {
+		frame.Stack.Push(NewAtomValueError(
+			FormatError(frame, "log expects 1 argument"),
+		))
+		return
+	}
+
+	arg := frame.Stack.Pop()
+
+	if !IsNumberType(arg) {
+		frame.Stack.Push(NewAtomValueError(
+			FormatError(frame, "log expects number"),
+		))
+		return
+	}
+
+	val := CoerceToNum(arg)
+	frame.Stack.Push(NewAtomValueNum(math.Log(val)))
+})
+
 var EXPORT_MATH = map[string]*AtomValue{
-	"rand": NewAtomValueNativeFunc(math_rand),
+	"rand":  NewAtomValueNativeFunc(math_rand),
+	"abs":   NewAtomValueNativeFunc(math_abs),
+	"floor": NewAtomValueNativeFunc(math_floor),
+	"ceil":  NewAtomValueNativeFunc(math_ceil),
+	"round": NewAtomValueNativeFunc(math_round),
+	"pow":   NewAtomValueNativeFunc(math_pow),
+	"sqrt":  NewAtomValueNativeFunc(math_sqrt),
+	"log":   NewAtomValueNativeFunc(math_log),
 }
