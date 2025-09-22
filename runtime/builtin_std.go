eaa00927a2e5b600c13a312b2f91aacb08e314be
HolliShake <redondophilippandrewroa.dev@gmail.com>
2025-09-22 20:22:11 +0800

    Enhance output handling in runtime and add Person class to tests
    - Updated the `println` and `print` functions to use buffered output for improved performance and reduced console I/O overhead.
    - Added a new `Person` class with an initializer method to the test class, demonstrating object creation and usage.
    - Included a loop in the test class to instantiate multiple `Person` objects, showcasing the new class functionality.

--- runtime/builtin_std.go
+++ runtime/builtin_std.go
@@ -27,23 +27,28 @@ var std_decompile = NewNativeFunc("decompile", 1, func(interpreter *AtomInterpre
 })
 
 var std_println = NewNativeFunc("println", Variadict, func(interpreter *AtomInterpreter, frame *AtomCallFrame, argc int) {
+	writer := bufio.NewWriter(os.Stdout)
 	for i := range argc {
-		fmt.Print(color.YellowString(frame.Stack.Pop().String()))
+		fmt.Fprint(writer, color.YellowString(frame.Stack.Pop().String()))
 		if i < argc-1 {
-			fmt.Print(" ")
+			fmt.Fprint(writer, " ")
 		}
 	}
-	fmt.Println()
+	fmt.Fprintln(writer)
+	writer.Flush()
 	frame.Stack.Push(interpreter.State.NullValue)
 })
 
 var std_print = NewNativeFunc("print", Variadict, func(interpreter *AtomInterpreter, frame *AtomCallFrame, argc int) {
+	writer := bufio.NewWriter(os.Stdout)
 	for i := range argc {
-		fmt.Print(color.GreenString(frame.Stack.Pop().String()))
+		fmt.Fprint(writer, color.YellowString(frame.Stack.Pop().String()))
 		if i < argc-1 {
-			fmt.Print(" ")
+			fmt.Fprint(writer, " ")
 		}
 	}
+	fmt.Fprint(writer)
+	writer.Flush()
 	frame.Stack.Push(interpreter.State.NullValue)
 })
 
